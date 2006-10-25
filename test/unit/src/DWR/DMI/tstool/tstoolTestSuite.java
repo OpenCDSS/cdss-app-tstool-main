2ef1c89b2a4b82cb3e6a35973eda71d5416e4de6
automatic import <auto@placeholder.com>
2006-10-25 16:44:21 +0000

    Added unit testing capability to TSTool
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@27 1ecda906-20c6-4f4e-8c99-029b5e969ef8

---
+++ test/unit/src/DWR/DMI/tstool/tstoolTestSuite.java
@@ -0,0 +1,45 @@
+package DWR.DMI.tstool;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import RTi.Util.Test.TestCollector;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+public class tstoolTestSuite extends TestCase {
+
+	private static ArrayList testList;
+	
+	public tstoolTestSuite(String testname)
+	{
+		super(testname);
+	}
+
+	public tstoolTestSuite()
+	{	
+	}	
+	
+	public static Test suite() throws ClassNotFoundException
+	{
+		testList = new ArrayList();
+	    TestSuite suite = new TestSuite();
+	    TestCollector tests = new TestCollector();
+	    File path = new File("..\\test\\unit\\src");
+	    tests.visitAllFiles(path);
+	    testList = tests.getTestList();
+	    
+	    for(int i = 0; i < testList.size(); i++)
+	    {
+	    	String testName = (testList.get(i).toString());
+	    	String test = tests.formatFileName(testName);
+	    	suite.addTestSuite(Class.forName(test));
+	    }
+		
+	    return suite;
+	}
+	
+	
+}
+
