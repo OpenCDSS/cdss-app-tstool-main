371b66fd4dc692baec259c2b450deaaf7e790781
Steve Malers <steve.malers@openwaterfoundation.org>
2008-01-19 23:34:48 +0000

    Update to TSTool 08.13.01 - add day of week to metrics report to track down incomplete output.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@208 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/tstool.java
+++ src/DWR/DMI/tstool/tstool.java
@@ -443,7 +443,7 @@ or run the TSCommandProcessor in batch mode with a command file.
 public class tstool extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "8.13.00beta (2008-01-18)";
+public static final String PROGRAM_VERSION = "8.13.01beta (2008-01-19)";
 
 /**
 Main GUI instance, used when running interactively.
