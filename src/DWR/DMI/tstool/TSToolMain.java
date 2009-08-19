126e339f33ea957378df63cdc009b4923139e551
Steve Malers <steve.malers@openwaterfoundation.org>
2009-08-19 20:50:07 +0000

    Update software version.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@509 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -455,7 +455,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "9.04.02 beta (2009-07-22)";
+public static final String PROGRAM_VERSION = "9.04.03 beta (2009-08-19)";
 
 /**
 Main GUI instance, used when running interactively.
