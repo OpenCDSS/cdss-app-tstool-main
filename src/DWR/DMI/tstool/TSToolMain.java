90814772fcd0893c05b8a373d673c847b0c75abb
Steve Malers <steve.malers@openwaterfoundation.org>
2009-09-18 18:44:20 +0000

    Incremental work used with FillMixedStation() command - still runs too long and runs out of memory.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@512 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -455,7 +455,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "9.04.03 beta (2009-08-19)";
+public static final String PROGRAM_VERSION = "9.04.03 beta (2009-08-31)";
 
 /**
 Main GUI instance, used when running interactively.
