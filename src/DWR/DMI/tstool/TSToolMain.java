05e18d651cda69a058ee8312a17e1163f60d0dd0
Steve Malers <steve.malers@openwaterfoundation.org>
2009-10-23 10:50:30 +0000

    Update NewStatisticTimeSeries command and associated supported code to work on a sample of values to facilitate adding new statistics.  Pull the command functionality out of TSAnalyst and deprecate old methods.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@518 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -455,7 +455,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "9.05.00 beta (2009-10-15)";
+public static final String PROGRAM_VERSION = "9.05.01 beta (2009-10-19)";
 
 /**
 Main GUI instance, used when running interactively.
