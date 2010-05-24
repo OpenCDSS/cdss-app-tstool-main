9ff6c3a0451d95d3af842543f473485cce328849
Steve Malers <steve.malers@openwaterfoundation.org>
2010-05-24 16:33:19 +0000

    Create Python script to transpose CSV file rows/columns - useful to overcome Excel 256 column limitation and format data for TSTool, whice expects column of date/times.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@544 1ecda906-20c6-4f4e-8c99-029b5e969ef8

---
+++ resources/runtime/python/table/transpose-csv.py
@@ -0,0 +1,46 @@
+#
+# Simple Python script to transpose a CSV file so that rows are columns.
+# This is useful to get around the Excel 256 column limit.
+# There is very little error checking in this script.
+#
+# 1. The input is allowed to have # comments at the top - they are discarded.
+# 2. The individual cell contents are trimmed (leading and trailing whitespace is removed).
+# 3. Filenames with \ in the names replace with \\ for internal use.
+#
+# Usage:  python transpose-csv.py infile.csv outfile.csv
+#
+# Steve Malers, 2010-05-21
+
+import re
+import sys
+
+# Get the file names - assume incoming paths may have single \ but need \\ internally
+infile = sys.argv[1].replace('\\','\\\\')
+outfile = sys.argv[2].replace('\\','\\\\')
+
+# Read through the input file and put into a 2-dimensional array
+delim = ","
+cells = []
+inf = open(infile,"r")
+nrows = 0
+for line in inf:
+	if ( line.startswith("#") ):
+		# Comment
+		continue
+	cells.append(re.split(delim,line.strip()))
+	nrows = nrows + 1
+inf.close()
+
+# Write the 2-D array in transposed fashion.
+# Assume that the number of columns is consistent
+
+outf = open(outfile,"w")
+ncols = len(cells[0])
+print ( "Read file with " + str(nrows) + " rows and " + str(ncols) + " columns" )
+for icol in range(0,ncols):
+	for irow in range(0,nrows):
+		if ( irow > 0 ):
+			outf.write(delim)
+		outf.write(cells[irow][icol].strip())
+	outf.write("\n")
+outf.close()
