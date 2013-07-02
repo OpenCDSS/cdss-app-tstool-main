bbe0fd4aa84925ad6adfc3b5e16b8aa2b9a9d2ba
Steve Malers <steve.malers@openwaterfoundation.org>
2013-07-02 06:07:25 +0000

    Show the time series processor properties in the results area.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@791 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/TSTool_JFrame.java
+++ src/DWR/DMI/tstool/TSTool_JFrame.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
@@ -223,6 +224,8 @@ import RTi.Util.IO.FileGenerator;
 import RTi.Util.IO.HTMLViewer;
 import RTi.Util.IO.IOUtil;
 import RTi.Util.IO.LicenseManager;
+import RTi.Util.IO.PropList_CellRenderer;
+import RTi.Util.IO.PropList_TableModel;
 import RTi.Util.IO.TextPrinterJob;
 import RTi.Util.IO.ProcessManager;
 import RTi.Util.IO.Prop;
@@ -236,6 +239,7 @@ import RTi.Util.Message.MessageLogListener;
 import RTi.Util.String.StringUtil;
 import RTi.Util.Table.DataTable;
 import RTi.Util.Table.DataTable_JFrame;
+import RTi.Util.Table.DataTable_JPanel;
 import RTi.Util.Table.TableColumnType;
 import RTi.Util.Table.TableField;
 import RTi.Util.Table.TableRecord;
@@ -696,6 +700,11 @@ private JPopupMenu __resultsTSEnsembles_JPopupMenu = null;
 Worksheet that contains a list of problems from processing.
 */
 private JWorksheet __resultsProblems_JWorksheet = null;
+
+/**
+Worksheet that contains a list of processor properties.
+*/
+private JWorksheet __resultsProperties_JWorksheet = null;
 
 /**
 Panel for TS results.
@@ -7648,6 +7657,41 @@ private void ui_InitGUI ( )
     JGUIUtil.addComponent(results_problems_JPanel, sjw, 
         0, 0, 8, 5, 1.0, 1.0, insetsNLNR, GridBagConstraints.BOTH, GridBagConstraints.WEST);
     __results_JTabbedPane.addTab ( "Problems", results_problems_JPanel );
+    
+    // Results - properties...
+
+    JPanel resultsProperties_JPanel = new JPanel();
+    resultsProperties_JPanel.setLayout(gbl);
+    PropList_TableModel propsTableModel = null;
+    try {
+        propsTableModel = new PropList_TableModel(new PropList("processor"),false,false);
+        propsTableModel.setKeyColumnName("Property Name");
+        propsTableModel.setValueColumnName("Property Value");
+    }
+    catch ( Exception e ) {
+        // Should not happen but log
+        Message.printWarning ( 3, routine, e );
+        Message.printWarning(3, routine, "Error creating table model for problem display.");
+        throw new RuntimeException ( e );
+    }
+    PropList_CellRenderer propsCellRenderer = new PropList_CellRenderer(propsTableModel);
+    PropList propsWsProps = new PropList ( "PropertiesWS" );
+    propsWsProps.add("JWorksheet.ShowRowHeader=true");
+    propsWsProps.add("JWorksheet.AllowCopy=true");
+    // Initialize with null table model since no initial data
+    JScrollWorksheet psjw = new JScrollWorksheet ( propsCellRenderer, propsTableModel, propsWsProps );
+    __resultsProperties_JWorksheet = psjw.getJWorksheet ();
+    __resultsProperties_JWorksheet.setColumnWidths (cellRenderer.getColumnWidths(), getGraphics() );
+    __resultsProperties_JWorksheet.setPreferredScrollableViewportSize(null);
+    // Listen for mouse events to ??...
+    //__problems_JWorksheet.addMouseListener ( this );
+    //__problems_JWorksheet.addJWorksheetListener ( this );
+    JGUIUtil.addComponent(resultsProperties_JPanel, new JLabel("Processor properties control processing and can be used in " +
+        "some command parameters using ${PropertyName} notation (see command documentation)."), 
+        0, 0, 8, 1, 0.0, 0.0, insetsNLNR, GridBagConstraints.NONE, GridBagConstraints.WEST);
+    JGUIUtil.addComponent(resultsProperties_JPanel, psjw, 
+        0, 1, 8, 5, 1.0, 1.0, insetsNLNR, GridBagConstraints.BOTH, GridBagConstraints.WEST);
+    __results_JTabbedPane.addTab ( "Properties", resultsProperties_JPanel );
     
     // Results: tables...
 
@@ -16606,6 +16650,7 @@ private void uiAction_RunCommands_ShowResults()
             uiAction_RunCommands_ShowResultsEnsembles();
             uiAction_RunCommands_ShowResultsOutputFiles();
             uiAction_RunCommands_ShowResultsProblems();
+            uiAction_RunCommands_ShowResultsProperties();
             uiAction_RunCommands_ShowResultsTables();
 			uiAction_RunCommands_ShowResultsTimeSeries();
 			uiAction_RunCommands_ShowResultsViews();
@@ -16720,6 +16765,42 @@ private void uiAction_RunCommands_ShowResultsProblems()
             ") - contact support.");
     }
     //Message.printStatus ( 2, "uiAction_RunCommands_ShowProblems", "Leaving method.");
+}
+
+/**
+Display the list of properties from the command processor.
+*/
+private void uiAction_RunCommands_ShowResultsProperties()
+{   String routine = getClass().getName() + ".uiAction_RunCommands_ShowResultsProperties";
+    try {
+        // Create a new table model for the command processor properties.
+        // TODO SAM 2009-03-01 Evaluate whether should just update data in existing table model (performance?)
+        TSCommandProcessor processor = commandProcessor_GetCommandProcessor();
+        Collection<String> propertyNames = processor.getPropertyNameList(true, true);
+        PropList props = new PropList("processor");
+        Object propVal;
+        for ( String propertyName : propertyNames ) {
+            propVal = processor.getPropContents(propertyName);
+            if ( propVal == null) {
+                props.set(new Prop(propertyName, propVal, ""));
+            }
+            else {
+                props.set(new Prop(propertyName, propVal, "" + processor.getPropContents(propertyName) ) );
+            }
+        }
+        PropList_TableModel tableModel = new PropList_TableModel ( props, false, false );
+        tableModel.setKeyColumnName("Property Name");
+        tableModel.setValueColumnName("Property Value");
+        PropList_CellRenderer cellRenderer = new PropList_CellRenderer( tableModel );
+        __resultsProperties_JWorksheet.setCellRenderer ( cellRenderer );
+        __resultsProperties_JWorksheet.setModel ( tableModel );
+        __resultsProperties_JWorksheet.setColumnWidths ( cellRenderer.getColumnWidths() );
+        ui_SetIgnoreActionEvent(false);
+    }
+    catch ( Exception e ) {
+        Message.printWarning( 3 , routine, e);
+        Message.printWarning ( 1, routine, "Unexpected error displaying processor properties (" + e + ") - contact support.");
+    }
 }
 
 /**
