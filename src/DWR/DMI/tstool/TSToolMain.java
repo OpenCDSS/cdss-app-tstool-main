9a9c79e10391d2853396beea49c39af33fce83a1
Steve Malers <steve.malers@openwaterfoundation.org>
2013-09-09 04:12:36 +0000

    Save increment of work for CreateTimeSeriesEventTable - mostly functional now and ready to test with graphing.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-main/java_142/trunk@810 1ecda906-20c6-4f4e-8c99-029b5e969ef8

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -466,7 +466,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "10.22.02 (2013-09-08)";
+public static final String PROGRAM_VERSION = "10.23.00 (2013-09-08)";
 
 /**
 Main GUI instance, used when running interactively.
