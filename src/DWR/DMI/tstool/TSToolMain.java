0c096e8133b9a170cac0f6d41519bbeea8afb8a3
Steve Malers <steve.malers@openwaterfoundation.org>
2015-06-29 04:23:41 -0600

    Update TSTool version to 11.03.10 for public release.

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -470,7 +470,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "11.03.09beta (2015-06-25)";
+public static final String PROGRAM_VERSION = "11.03.10 (2015-06-29)";
 
 /**
 Main GUI instance, used when running interactively.
