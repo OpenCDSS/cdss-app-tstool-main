db89177b0e9490661f1acaaab6df479acb9e7589
Steve Malers <steve.malers@openwaterfoundation.org>
2015-08-28 11:57:39 -0600

    Add Pisces start, end, and count columns to time series table list.

--- src/DWR/DMI/tstool/TSTool_ReclamationPisces_TableModel.java
+++ src/DWR/DMI/tstool/TSTool_ReclamationPisces_TableModel.java
@@ -5,7 +5,9 @@ import java.util.List;
 import rti.tscommandprocessor.commands.reclamationpisces.ReclamationPiscesDMI;
 import rti.tscommandprocessor.commands.reclamationpisces.ReclamationPiscesDataStore;
 import rti.tscommandprocessor.commands.reclamationpisces.ReclamationPisces_SiteCatalogSeriesCatalog;
+import RTi.DMI.DMIUtil;
 import RTi.Util.GUI.JWorksheet_AbstractRowTableModel;
+import RTi.Util.Time.DateTime;
 
 /**
 This class is a table model for time series header information for Reclamation Pisces
@@ -20,6 +22,7 @@ Number of columns in the table model.
 private final int COLUMNS = 30;
 
 //Karl Tarbet said not to display the following (2015-08-26)...
+//public final int COL_SERIES_PROVIDER = 24;
 //public final int COL_SERIES_ID = 25; 
 //public final int COL_SERIES_PARENTID = 26;
 //public final int COL_SERIES_ISFOLDER = 27;
@@ -35,22 +38,22 @@ public final int COL_SERIES_TIMEINTERVAL = 5;
 public final int COL_SERIES_UNITS = 6;
 public final int COL_SERIES_START = 7;
 public final int COL_SERIES_END = 8;
-public final int COL_SITE_STATE = 9;
-public final int COL_SITE_AGENCY_REGION = 10;
-public final int COL_SITE_LONGITUDE = 11;
-public final int COL_SITE_LATITUDE = 12;
-public final int COL_SITE_HORIZONTAL_DATUM = 13;
-public final int COL_SITE_ELEVATION = 14;
-public final int COL_SITE_VERTICAL_DATUM = 15;
-public final int COL_SITE_ELEVATION_METHOD = 16;
-public final int COL_SITE_VERTICAL_ACCURACY = 17;
-public final int COL_SITE_TIMEZONE = 18;
-public final int COL_SITE_TIMEZONE_OFFSET = 19;
-public final int COL_SITE_INSTALL = 20;
-public final int COL_SITE_ACTIVE_FLAG = 21;
-public final int COL_SITE_TYPE = 22;
-public final int COL_SITE_RESPONSIBILITY = 23;
-public final int COL_SERIES_PROVIDER = 24;
+public final int COL_SERIES_COUNT = 9;
+public final int COL_SITE_STATE = 10;
+public final int COL_SITE_AGENCY_REGION = 11;
+public final int COL_SITE_LONGITUDE = 12;
+public final int COL_SITE_LATITUDE = 13;
+public final int COL_SITE_HORIZONTAL_DATUM = 14;
+public final int COL_SITE_ELEVATION = 15;
+public final int COL_SITE_VERTICAL_DATUM = 16;
+public final int COL_SITE_ELEVATION_METHOD = 17;
+public final int COL_SITE_VERTICAL_ACCURACY = 18;
+public final int COL_SITE_TIMEZONE = 19;
+public final int COL_SITE_TIMEZONE_OFFSET = 20;
+public final int COL_SITE_INSTALL = 21;
+public final int COL_SITE_ACTIVE_FLAG = 22;
+public final int COL_SITE_TYPE = 23;
+public final int COL_SITE_RESPONSIBILITY = 24;
 public final int COL_SERIES_ICONNAME = 25;
 public final int COL_SERIES_TABLENAME = 26;
 public final int COL_SERIES_CONNECTION_STRING = 27;
@@ -88,6 +91,9 @@ From AbstractTableModel.  Returns the class of the data stored in a given column
 */
 public Class getColumnClass (int columnIndex) {
 	switch (columnIndex) {
+		case COL_SERIES_START: return DateTime.class;
+		case COL_SERIES_END: return DateTime.class;
+		case COL_SERIES_COUNT: return Integer.class;
 		case COL_SITE_LATITUDE: return Double.class;
 		case COL_SITE_LONGITUDE: return Double.class;
 		case COL_SITE_ELEVATION: return Double.class;
@@ -119,6 +125,7 @@ public String getColumnName(int columnIndex) {
 		case COL_SERIES_UNITS: return "Units";
 		case COL_SERIES_START: return "Start";
 		case COL_SERIES_END: return "End";
+		case COL_SERIES_COUNT: return "Count";
 		case COL_SITE_STATE: return "State";
 		case COL_SITE_AGENCY_REGION: return "Region";
 		case COL_SITE_LONGITUDE: return "Longitude";
@@ -134,7 +141,6 @@ public String getColumnName(int columnIndex) {
 		case COL_SITE_ACTIVE_FLAG: return "Site Active Flag";
 		case COL_SITE_TYPE: return "Site Type";
 		case COL_SITE_RESPONSIBILITY: return "Site Responsibility";
-		case COL_SERIES_PROVIDER: return "Series Provider";
 		case COL_SERIES_ICONNAME: return "Series Icon Name";
 		case COL_SERIES_TABLENAME: return "Series Table Name";
 		case COL_SERIES_CONNECTION_STRING: return "Series Connection String";
@@ -151,6 +157,7 @@ Returns the format to display the specified column.
 */
 public String getFormat ( int column ) {
 	switch (column) {
+		case COL_SERIES_COUNT: return "%d";
 		case COL_SITE_LATITUDE: return "%.6f";
 		case COL_SITE_LONGITUDE: return "%.6f";
 		case COL_SITE_ELEVATION: return "%.2f";
@@ -181,22 +188,22 @@ public String[] getColumnToolTips() {
     toolTips[COL_SERIES_UNITS] = "Data units of measure";
     toolTips[COL_SERIES_START] = "Starting date/time for the series";
     toolTips[COL_SERIES_END] = "Ending date/time for the series";
+    toolTips[COL_SERIES_COUNT] = "Count of observations/measurements";
     toolTips[COL_SITE_STATE] = "State abbreviation";
     toolTips[COL_SITE_AGENCY_REGION] = "Reclamation agency region";
     toolTips[COL_SITE_LONGITUDE] = "Site latitude, decimal degrees";
     toolTips[COL_SITE_LATITUDE] = "Site longitude, decimal degrees";
-    toolTips[COL_SITE_HORIZONTAL_DATUM] = "??";
-    toolTips[COL_SITE_ELEVATION] = "??";
-    toolTips[COL_SITE_VERTICAL_DATUM] = "??";
-    toolTips[COL_SITE_ELEVATION_METHOD] = "??";
-    toolTips[COL_SITE_VERTICAL_ACCURACY] = "??";
-    toolTips[COL_SITE_TIMEZONE] = "??";
-    toolTips[COL_SITE_TIMEZONE_OFFSET] = "??";
+    toolTips[COL_SITE_HORIZONTAL_DATUM] = "Horizontal datum for the site (e.g., \"WGS84\")";
+    toolTips[COL_SITE_ELEVATION] = "Elevation of the site in units corresponding to the datum";
+    toolTips[COL_SITE_VERTICAL_DATUM] = "Vertical datum for the site (e.g., \"NGVD29\")";
+    toolTips[COL_SITE_ELEVATION_METHOD] = "Method used to determine elevation";
+    toolTips[COL_SITE_VERTICAL_ACCURACY] = "Accuracy of the elevation, in the units of the elevation";
+    toolTips[COL_SITE_TIMEZONE] = "Full time zone name for site, e.g., US/Pacific";
+    toolTips[COL_SITE_TIMEZONE_OFFSET] = "Time zone offset from UTC for site, e.g., -08:00 or -8";
     toolTips[COL_SITE_INSTALL] = "Date that site was installed";
-    toolTips[COL_SITE_ACTIVE_FLAG] = "??";
+    toolTips[COL_SITE_ACTIVE_FLAG] = "Indicates whether site is currently used, T/F (default T)";
     toolTips[COL_SITE_TYPE] = "Location type (e.g., reservoir)";
-    toolTips[COL_SITE_RESPONSIBILITY] = "??";
-    toolTips[COL_SERIES_PROVIDER] = "Name of class derived from Reclamation.TimeSeries.Series (or Series)";
+    toolTips[COL_SITE_RESPONSIBILITY] = "Maintenance responsibility";
     toolTips[COL_SERIES_ICONNAME] = "Used to render an icon based on the source of the data";
     toolTips[COL_SERIES_TABLENAME] = "Name of Pisces data table for time series";
     toolTips[COL_SERIES_CONNECTION_STRING] = "Provider-specific connection information such as path to Excel file, sheet name, or specific parameter code";
@@ -228,8 +235,9 @@ public Object getValueAt(int row, int col) {
 			ReclamationPiscesDMI dmi = (ReclamationPiscesDMI)this.datastore.getDMI();
 			return dmi.getTSIDIntervalFromPiscesInterval(data.getTimeInterval());
 		case COL_SERIES_UNITS: return data.getUnits();
-		case COL_SERIES_START: return "";
-		case COL_SERIES_END: return "";
+		case COL_SERIES_START: return data.getT1();
+		case COL_SERIES_END: return data.getT2();
+		case COL_SERIES_COUNT: return DMIUtil.isMissing(data.getCount()) ? null : data.getCount();
 		case COL_SITE_STATE: return data.getState();
 		case COL_SITE_AGENCY_REGION: return data.getAgencyRegion();
 		case COL_SITE_LONGITUDE: return data.getLongitude();
@@ -245,7 +253,6 @@ public Object getValueAt(int row, int col) {
 		case COL_SITE_ACTIVE_FLAG: return data.getActiveFlag();
 		case COL_SITE_TYPE: return data.getType();
 		case COL_SITE_RESPONSIBILITY: return data.getResponsibility();
-		case COL_SERIES_PROVIDER: return data.getProvider();
 		case COL_SERIES_ICONNAME: return data.getIconName();
 		case COL_SERIES_TABLENAME: return data.getTableName();
 		case COL_SERIES_CONNECTION_STRING: return data.getConnectionString();
@@ -268,8 +275,9 @@ public int[] getColumnWidths() {
     widths[COL_SERIES_PARAMETER] = 25;
     widths[COL_SERIES_TIMEINTERVAL] = 6;
     widths[COL_SERIES_UNITS] = 8;
-    widths[COL_SERIES_START] = 3;
-    widths[COL_SERIES_END] = 3;
+    widths[COL_SERIES_START] = 8;
+    widths[COL_SERIES_END] = 8;
+    widths[COL_SERIES_COUNT] = 3;
     widths[COL_SITE_STATE] = 3;
     widths[COL_SITE_AGENCY_REGION] = 10;
     widths[COL_SITE_LONGITUDE] = 7;
@@ -284,7 +292,6 @@ public int[] getColumnWidths() {
     widths[COL_SITE_INSTALL] = 7;
     widths[COL_SITE_ACTIVE_FLAG] = 11;
     widths[COL_SITE_TYPE] = 7;
-    widths[COL_SERIES_PROVIDER] = 13;
     widths[COL_SITE_RESPONSIBILITY] = 13;
     widths[COL_SERIES_ICONNAME] = 12;
     widths[COL_SERIES_TABLENAME] = 20;
