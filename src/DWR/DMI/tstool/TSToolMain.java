20fb734a0005d3e35fa3d574ff626618b1213e44
Steve Malers <steve.malers@openwaterfoundation.org>
2016-02-17 02:54:56 -0700

    Update TSTool version to 11.08.01 and add -httpServer option.

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -473,7 +473,7 @@ this file are called by the startup TSTool and CDSS versions of TSTool.
 public class TSToolMain extends JApplet
 {
 public static final String PROGRAM_NAME = "TSTool";
-public static final String PROGRAM_VERSION = "11.08.01beta (2016-02-15)";
+public static final String PROGRAM_VERSION = "11.08.01 (2016-02-15)";
 
 /**
 Main GUI instance, used when running interactively.
