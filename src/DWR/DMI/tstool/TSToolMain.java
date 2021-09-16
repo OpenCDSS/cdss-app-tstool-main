d07e47a1751059b52768b076af56ba829210a773
Steve Malers <steve.malers@openwaterfoundation.org>
2021-09-16 16:39:17 -0600

    Update the version to for ongoing development, issue #178

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -94,7 +94,7 @@ public static final String PROGRAM_NAME = "TSTool";
  * - otherwise, there can be problems with the string being interpreted as hex code by installer tools
  * - as of version 14, do not pad version parts with zeros
  */
-public static final String PROGRAM_VERSION = "14.0.0 (2021-09-14)";
+public static final String PROGRAM_VERSION = "14.0.1.dev1 (2021-09-16)";
 
 /**
 Main GUI instance, used when running interactively.
