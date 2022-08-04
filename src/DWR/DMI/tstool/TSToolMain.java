cad00114a005e20343708460a187e46d38461acc
Steve Malers <steve.malers@openwaterfoundation.org>
2022-08-04 12:48:27 -0600

    Update version to 14.3.0 for release, issue #223

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -95,7 +95,7 @@ public static final String PROGRAM_NAME = "TSTool";
  * - otherwise, there can be problems with the string being interpreted as hex code by installer tools
  * - as of version 14, do not pad version parts with zeros
  */
-public static final String PROGRAM_VERSION = "14.3.0.dev3 (2022-07-12)";
+public static final String PROGRAM_VERSION = "14.3.0 (2022-08-01)";
 
 /**
 Main GUI instance, used when running interactively.
