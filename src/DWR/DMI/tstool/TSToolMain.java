b01ca8f2ec41494446e3550f5a258b51ea178ca7
Steve Malers <steve.malers@openwaterfoundation.org>
2025-01-23 04:06:09 -0700

    Update the version to 14.10.0 for release, issue #295

--- src/DWR/DMI/tstool/TSToolMain.java
+++ src/DWR/DMI/tstool/TSToolMain.java
@@ -4,7 +4,7 @@
 
 TSTool
 TSTool is a part of Colorado's Decision Support Systems (CDSS)
-Copyright (C) 1994-2024 Colorado Department of Natural Resources
+Copyright (C) 1994-2025 Colorado Department of Natural Resources
 
 TSTool is free software:  you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -101,7 +101,7 @@ public class TSToolMain
  * - otherwise, there can be problems with the string being interpreted as hex code by installer tools
  * - as of version 14, do not pad version parts with zeros
  */
-public static final String PROGRAM_VERSION = "14.10.0 (2024-10-29)";
+public static final String PROGRAM_VERSION = "14.10.0 (2025-01-23)";
 
 /**
 Main GUI instance, used when running interactively.
